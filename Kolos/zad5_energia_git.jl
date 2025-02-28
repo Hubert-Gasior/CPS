51d6a9671ca61d30c091730c1d6e79a28b04d2e2
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-02-28 16:24:58 +0100

    Update zad5_energia_git.jl

--- Kolos/zad5_energia_git.jl
+++ Kolos/zad5_energia_git.jl
@@ -3,44 +3,17 @@
 ##Pierwsza próbka 𝑥1=𝑦(𝑡1) została pobrana w chwili 𝑡1=−0.76. Funkcja 𝑔(𝑡) zwraca wartości sygnału fali piłokształtnej o opadającym zboczu
 ##i następujących parametrach: amplituda 1, okres 1 sekunda, składowa stała 0, 𝑔(0)=0, oraz 𝑑𝑔𝑑𝑡∣𝑡=0=−1.
 
-function rozwiazanie(;
-
-    #wynik -0.04714155615918824, dg/dt = 4 fala trojkatna y(t) = 3.5*g(4.7*t-4.9) git -0.0471415561591897
+function energy(;
     fp::Float64 = 325.49,
     t1::Float64 = 9.7,
     N::Int = 919
-
-    #wynik 1.6091548952441304, dg/dt = 2 pila o narastajacym zboczu y(t) = 2.2*g(1.2*t-2.9)
-    #fp::Float64 = 167.74,
-    #t1::Float64 = 2.14,
-    #N::Int = 998,
-
-    ## wynik 0.011251045424347012, dg/dt = -2, pila o poadajcym zboczu y(t) = 3.6*g(2.6*t - 1.0) obliczyc srednia sygnału
-    #fp::Float64 = 156.14,
-    #t1::Float64 = -6.97,
-    #N::Int = 860,
-
-    #wynik 10084.199999999997 , git 10084.199999999997 g(t) c(0, 1/2), y(t) = 4.9*g(2.4*t -2.9) bipolarna fala prostokatna, obliczyc enregie
-    #fp::Float64 = 118.8,
-    #t1::Float64 = -3.76,
-    #N::Int = 420,
-
-    ##wynik: 1046.577596197517 git 1046.5775961975173.
-    #fp::Float64 = 338.65,
-    #t1::Float64 = -0.76,
-    #N::Int = 130,
 )
-    #g = t -> -2 * rem(t, 1, RoundNearest) #piła o opadjacym zboczu git
-    #g = t -> ifelse(mod(t, 1) < 0.5, 1, -1) #bipolarny prostokat git
+    #g = t -> -2 * rem(t, 1, RoundNearest) #piła o opadjacym zboczu 
+    #g = t -> ifelse(mod(t, 1) < 0.5, 1, -1) #bipolarny prostokat 
     g = t -> 4*abs(t-floor(t+3/4)+1/4)-1
     t = range(; start=t1, step=(1 / fp), length=N)
-    #y = 3.4 * g.(0.8 .* t .- 3.1)
-    #y = 4.9*g.(2.4 .*t .-2.9)
-    #y = 3.6*g.(2.6.*t .- 1.0)
     y = 3.5*g.(4.7 .* t .- 4.9)
     energy = sum(abs2, y)
     mean = sum(y)/length(y)
     return mean
 end
-
-println(rozwiazanie())
